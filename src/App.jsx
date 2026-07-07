140e6565d68b600cd13164f910c5288d7867e90b
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-07 14:54:57 -0500

    Adding todo list object per instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
 
 function App() {
-
+  // Adding array todoList with 3 empty objects
+  const todoList = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+  ]
+  // keys are id and title
   return (
     <div>
       <h1>My Todos</h1>
