2fcf48abb5b0824df1c0a3fee1796837e7abc026
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-14 21:36:04 -0500

    Adding TodoList.jsx file with instructions / psuedo code

---
+++ src/TodoList.jsx
@@ -0,0 +1,4 @@
+// Set up a basic React component structure with:
+// A function named TodoList
+// An return statement with an empty Fragment component (we'll replace this soon)
+// An export statement at the bottom
