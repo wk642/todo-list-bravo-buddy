004a27ced28f31c0fd545d215d6b54694ab2a2fb
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-14 21:52:36 -0500

    Intergrated TodoForm in between header and TodoList in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
+      <TodoForm />
       <TodoList />
     </div>
   )
