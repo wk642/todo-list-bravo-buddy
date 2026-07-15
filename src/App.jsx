d4eb0447d2c824ebe42c2c17e31ecebd60b6bb0f
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-14 21:44:04 -0500

    Moving the logic from App.jsx to TodoList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import './App.css'
 import TodoList from './TodoList';
 
 function App() {
-  // Adding array todoList with 3 empty objects
-  const todoList = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-  ]
-  // keys are id and title
   return (
     <div>
       <h1>Todo List</h1>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,19 @@
 // Set up a basic React component structure with:
 // A function named TodoList
 function TodoList() {
-    // An return statement with an empty Fragment component (we'll replace this soon)
-    return (
-        <></>
-    )
+  // Moving Todo Logic here
+  // Adding array todoList with 3 empty objects
+  const todoList = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+  ]
+
+  return (
+    <ul>
+      {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+    </ul>
+  )
 }
 
 // An export statement at the bottom
