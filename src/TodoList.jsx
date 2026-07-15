0f7ea545587d05b2ad86854f95278685680a8f0a
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-14 21:37:31 -0500

    Created TodoList function returning empty fragment for now

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,11 @@
 // Set up a basic React component structure with:
 // A function named TodoList
-// An return statement with an empty Fragment component (we'll replace this soon)
+function TodoList() {
+    // An return statement with an empty Fragment component (we'll replace this soon)
+    return (
+        <></>
+    )
+}
+
 // An export statement at the bottom
+export default TodoList;
