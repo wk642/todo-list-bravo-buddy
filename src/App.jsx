efd06d1b1993722f8fd98f3f731c2dcd2384ae4b
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-21 22:13:15 -0500

    Changed from todoList to todos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
-const todoList = [
+const todos = [
   {id: 1, title: "review resources"},
   {id: 2, title: "take notes"},
   {id: 3, title: "code out app"}
