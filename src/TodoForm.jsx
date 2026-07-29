47edaaf4ecf938a33117ad0e54d330c022eeef7e
Winnie Kelley <191692065+wk642@users.noreply.github.com>
2026-07-29 07:12:14 -0500

    Finished part 3

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,37 @@
-function TodoForm() {
+import { useRef } from 'react';
+
+function TodoForm({ onAddTodo }) {
+  const inputRef = useRef();
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+
+    console.log('Event object:', event);
+    console.log('Event target:', event.target);
+    console.log('Input value:', event.target.todoTitle.value);
+
+    // .trim prevents whitespace only todos
+    const todoTitle = event.target.todoTitle.value.trim();
+    if (todoTitle && todoTitle !== "") {
+      onAddTodo(todoTitle);
+      event.target.reset();
+      inputRef.current.focus();
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>Add Todo</button>
+      <input
+        ref={inputRef}
+        type="text"
+        id="todoTitle"
+        name="todoTitle"
+        placeholder={'Todo text'}
+        required
+      />
+      <button type="submit">
+        Add Todo
+      </button>
     </form>
   );
 }
